380efdf450e74f5c2220f2183e2f018b00d984f6
pjh951012 <pjh951012@hotmail.com>
2021-09-30 06:12:51 +0000

    make file update

--- uvm_benchmarks/auto_mkdir.py
+++ uvm_benchmarks/auto_mkdir.py
@@ -1,16 +1,29 @@
 import os
 
-path = './release/rodinia-3.1'
-file_list = os.listdir('release')
+path = './bin/8.0/release/'
+file_list = os.listdir(path)
+cmd = 'mkdir ' + path + 'rodinia-3.1'
+os.system(cmd)
 
 for file in file_list:
     file_ = file.replace('-rodinia-3.1', '')
 
 #    mv_cmd = 'mv ' + path+'/'+file + ' '+ path+'/'+file_
 #    os.system(mv_cmd)
-    dir_name = path + '/' + file_
+    dir_name = path + 'rodinia-3.1/' + file_
     cmd = 'mkdir ' + dir_name
     os.system(cmd)
   
-    mv_cmd = 'mv ' + './release/'+ file + ' '  + dir_name
+    mv_cmd = 'mv ' + path+ file + ' '  + dir_name + '/' + file_
     os.system(mv_cmd)
+
+    run_path = './src/cuda/rodinia/3.1/cuda/'
+    if file_ == 'srad_v1' or 'srad_v2':
+	run_path= './src/cuda/rodinia/3.1/cuda/srad/'
+	cpy_run = 'cp' + run_path + file_ +'/run' + ' ' + dir_name +'/.'
+	
+    elif file_ == 'particlefilter_float' or 'particlefilter_naive':
+	run_path = './src/cuda/rodinia/3.1/cud/particlefilter/'
+	cpy_run = 'cp' + run_path + file_ +'/run' + ' ' + dir_name +'/.'
+    else:
+    	cpy_run = 'cp' + run_path + file_ +'/run' + ' ' + dir_name +'/.'
